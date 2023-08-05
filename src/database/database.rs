1f410dedc8661f48489b1d84610b5a18c05edb23
siri <sirimhrzn@gmail.com>
2023-08-06 00:19:46 +0545

    removed old url

--- src/database/database.rs
+++ src/database/database.rs
@@ -7,8 +7,6 @@ pub struct DBstate {
 }
 
 pub async fn db_connection() -> DBstate {
-    let _db_url =
-        env::var("DATABASE_URL").expect("Database Url not found or invalid in environment");
     let host = env::var("DB_HOST").expect("Db host not found or invalid in environment");
     let username =
         env::var("DB_USERNAME").expect("Database UserName not found or invalid in environment");
