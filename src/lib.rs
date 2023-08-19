ef67e3bfb7b145f0314bf0528bdd97ac54cb98ac
Shirish Maharjan <sirimhrzn@gmail.com>
2023-08-19 16:25:27 +0545

    fixed cors

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,19 @@ mod database;
 mod error;
 mod handler;
 mod service;
-use axum::{routing::get, Router};
+use axum::{extract::State, http::HeaderValue, routing::get, Router};
 use database::database::{db_connection, DBstate};
 use handler::handler::{darazv1, hamro_bazarv1};
 use std::env;
+use tower_http::cors::CorsLayer;
 
 pub async fn server() {
     let database: DBstate = db_connection().await;
     let app = Router::new()
         .route("/api/daraz", get(darazv1))
         .route("/api/hamrobazar", get(hamro_bazarv1))
-        .with_state(database);
+        .with_state(database)
+        .layer(CorsLayer::new().allow_origin("*".parse::<HeaderValue>().unwrap()));
 
     let server_address = env::var("SERVER_ADDRESS").expect("Server Address not set in environment");
     let server_port = env::var("SERVER_PORT").expect("Server Port not set in environment");
